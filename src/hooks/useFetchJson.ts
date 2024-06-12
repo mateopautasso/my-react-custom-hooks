3d717c1ed8319cfa135d68c1d21a8795a569f043
mateopautasso <mateopautassodev@gmail.com>
2024-06-12 01:00:39 -0300

    refactor: :recycle: Reorganización de código en "useFetchJson".

--- src/hooks/useFetchJson.ts
+++ src/hooks/useFetchJson.ts
@@ -1,7 +1,6 @@
 import { useCallback, useEffect, useState } from 'react'
 
 export const useFetchJson = <T>(fetchingFn: () => Promise<Response>, initialCall = true) => {
-	//
 	const [loading, setLoading] = useState(() => (initialCall ? true : false))
 	const [data, setData] = useState<T | null>(null)
 	const [error, setError] = useState(false)
@@ -15,13 +14,10 @@ export const useFetchJson = <T>(fetchingFn: () => Promise<Response>, initialCall
 			if (!request.ok) throw new Error()
 			const response = await request.json()
 			setData(response)
-			//
 		} catch (error) {
 			setError(true)
-			//
 		} finally {
 			setLoading(false)
-			//
 		}
 	}, [fetchingFn])
 
